5dbb86ae7a208103e92f9495a2eab26d7b865443
Vokalhe <wot_@ukr.net>
2016-01-27 02:43:09 +0200

    Create Artists.h

---
+++ Artists.h
@@ -0,0 +1,16 @@
+//
+//  Artists.h
+//  Lesson2
+//
+//  Created by Admin on 26.01.16.
+//  Copyright © 2016 Ehlakov Victor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Artists : NSObject
+
+@property NSString *name;
+@property NSMutableArray *listOfSongs;
+
+@end
