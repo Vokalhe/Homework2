492c4d253426721fee389a696e665923c01a9336
Vokalhe <wot_@ukr.net>
2016-01-27 02:55:31 +0200

    Create NSArray+Utility.h

---
+++ NSArray+Utility.h
@@ -0,0 +1,17 @@
+//
+//  NSArray+Utility.h
+//  Lesson2
+//
+//  Created by Admin on 26.01.16.
+//  Copyright © 2016 Ehlakov Victor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "Artists.h"
+#import "Songs.h"
+#import "Group.h"
+
+@interface NSArray (Utility)
+-(NSString*) prettyView;
+@end
