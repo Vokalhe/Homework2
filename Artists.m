7e903a99951e6cd6ac3b81d8f1ed94fdaf801e7d
Vokalhe <wot_@ukr.net>
2016-01-27 02:51:11 +0200

    Create Artists.m

---
+++ Artists.m
@@ -0,0 +1,13 @@
+//
+//  Artists.m
+//  Lesson2
+//
+//  Created by Admin on 26.01.16.
+//  Copyright © 2016 Ehlakov Victor. All rights reserved.
+//
+
+#import "Artists.h"
+
+@implementation Artists
+
+@end
